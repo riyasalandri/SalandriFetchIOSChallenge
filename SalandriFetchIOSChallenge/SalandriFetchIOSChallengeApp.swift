696eec0305de17b539c2e883de0d4b5f8e259a6c
Abhishek <abhisheksalandri@gmail.com>
2023-09-19 14:55:43 -0400

    Initial Commit

---
+++ SalandriFetchIOSChallenge/SalandriFetchIOSChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SalandriFetchIOSChallengeApp.swift
+//  SalandriFetchIOSChallenge
+//
+//  Created by Abhishek Salandri on 9/19/23.
+//
+
+import SwiftUI
+
+@main
+struct SalandriFetchIOSChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
